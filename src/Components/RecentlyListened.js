2f90aa71ee5de55718d6375d98f12aa054abd244
Raphael Andrade <raphael.andrade@ibm.com>
2020-07-15 17:53:48 -0300

    resolution issues fixed

--- src/Components/RecentlyListened.js
+++ src/Components/RecentlyListened.js
@@ -14,44 +14,52 @@ function RecentlyListened() {
                     <div className="banner-recently">
                     <ul className="listMusic">
                           <li>
-                            <div className="albumMusic">
-                                <img src="https://images.squarespace-cdn.com/content/v1/56d1077e8a65e2c2b911c5fa/1524683962396-IP8LREI9L95WV6JEYWLU/ke17ZwdGBToddI8pDm48kNgFyjlEyNHlSWEjE-QCU1p7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QPOohDIaIeljMHgDF5CVlOqpeNLcJ80NK65_fV7S1UdLKTLgsLX9_T7LnpaostY9WYLb0IFNaX6bgMhY2dUNBWIB-7cQgYKo_bDpR6cEVkg/BA_ART_NoLimits%28tunecore%29.jpg" alt="Picture of Profile" className="img-fluid"/>
-                            </div>
-                            <div className="description">
-                                <span>Photograph</span>
-                                <label>Boyce Avenue</label>
-                                <span className="more">...</span>
-                            </div>
+                              <ul className="list-inline">
+                                <li className="list-inline-item">
+                                    <div className="albumMusic">
+                                        <img src="https://images.squarespace-cdn.com/content/v1/56d1077e8a65e2c2b911c5fa/1524683962396-IP8LREI9L95WV6JEYWLU/ke17ZwdGBToddI8pDm48kNgFyjlEyNHlSWEjE-QCU1p7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QPOohDIaIeljMHgDF5CVlOqpeNLcJ80NK65_fV7S1UdLKTLgsLX9_T7LnpaostY9WYLb0IFNaX6bgMhY2dUNBWIB-7cQgYKo_bDpR6cEVkg/BA_ART_NoLimits%28tunecore%29.jpg" alt="Picture of Profile" className="img-fluid"/>
+                                    </div>
+                                </li>
+                                <li className="list-inline-item">
+                                    <div className="description">
+                                        <span>Photograph</span>
+                                        <label>Boyce Avenue</label>
+                                        <span className="more">...</span>
+                                    </div>
+                                </li>
+                              </ul>
                           </li>
                           <li>
-                            <div className="albumMusic">
-                                <img src="https://images.squarespace-cdn.com/content/v1/56d1077e8a65e2c2b911c5fa/1524683962396-IP8LREI9L95WV6JEYWLU/ke17ZwdGBToddI8pDm48kNgFyjlEyNHlSWEjE-QCU1p7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QPOohDIaIeljMHgDF5CVlOqpeNLcJ80NK65_fV7S1UdLKTLgsLX9_T7LnpaostY9WYLb0IFNaX6bgMhY2dUNBWIB-7cQgYKo_bDpR6cEVkg/BA_ART_NoLimits%28tunecore%29.jpg" alt="Picture of Profile" className="img-fluid"/>
-                            </div>
-                            <div className="description">
-                                <span>Mirrors</span>
-                                <label>Boyce Avenue</label>
-                                <span className="more">...</span>
-                            </div>
+                              <ul className="list-inline">
+                                <li className="list-inline-item">
+                                    <div className="albumMusic">
+                                        <img src="https://images.squarespace-cdn.com/content/v1/56d1077e8a65e2c2b911c5fa/1524683962396-IP8LREI9L95WV6JEYWLU/ke17ZwdGBToddI8pDm48kNgFyjlEyNHlSWEjE-QCU1p7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QPOohDIaIeljMHgDF5CVlOqpeNLcJ80NK65_fV7S1UdLKTLgsLX9_T7LnpaostY9WYLb0IFNaX6bgMhY2dUNBWIB-7cQgYKo_bDpR6cEVkg/BA_ART_NoLimits%28tunecore%29.jpg" alt="Picture of Profile" className="img-fluid"/>
+                                    </div>
+                                </li>
+                                <li className="list-inline-item">
+                                    <div className="description">
+                                        <span>Photograph</span>
+                                        <label>Boyce Avenue</label>
+                                        <span className="more">...</span>
+                                    </div>
+                                </li>
+                              </ul>
                           </li>
                           <li>
-                            <div className="albumMusic">
-                                <img src="https://images.squarespace-cdn.com/content/v1/56d1077e8a65e2c2b911c5fa/1524683962396-IP8LREI9L95WV6JEYWLU/ke17ZwdGBToddI8pDm48kNgFyjlEyNHlSWEjE-QCU1p7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QPOohDIaIeljMHgDF5CVlOqpeNLcJ80NK65_fV7S1UdLKTLgsLX9_T7LnpaostY9WYLb0IFNaX6bgMhY2dUNBWIB-7cQgYKo_bDpR6cEVkg/BA_ART_NoLimits%28tunecore%29.jpg" alt="Picture of Profile" className="img-fluid"/>
-                            </div>
-                            <div className="description">
-                                <span>Demons</span>
-                                <label>Boyce Avenue</label>
-                                <span className="more">...</span>
-                            </div>
-                          </li>
-                          <li>
-                            <div className="albumMusic">
-                                <img src="https://images.squarespace-cdn.com/content/v1/56d1077e8a65e2c2b911c5fa/1524683962396-IP8LREI9L95WV6JEYWLU/ke17ZwdGBToddI8pDm48kNgFyjlEyNHlSWEjE-QCU1p7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QPOohDIaIeljMHgDF5CVlOqpeNLcJ80NK65_fV7S1UdLKTLgsLX9_T7LnpaostY9WYLb0IFNaX6bgMhY2dUNBWIB-7cQgYKo_bDpR6cEVkg/BA_ART_NoLimits%28tunecore%29.jpg" alt="Picture of Profile" className="img-fluid"/>
-                            </div>
-                            <div className="description">
-                                <span>Demons</span>
-                                <label>Boyce Avenue</label>
-                                <span className="more">...</span>
-                            </div>
+                              <ul className="list-inline">
+                                <li className="list-inline-item">
+                                    <div className="albumMusic">
+                                        <img src="https://images.squarespace-cdn.com/content/v1/56d1077e8a65e2c2b911c5fa/1524683962396-IP8LREI9L95WV6JEYWLU/ke17ZwdGBToddI8pDm48kNgFyjlEyNHlSWEjE-QCU1p7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QPOohDIaIeljMHgDF5CVlOqpeNLcJ80NK65_fV7S1UdLKTLgsLX9_T7LnpaostY9WYLb0IFNaX6bgMhY2dUNBWIB-7cQgYKo_bDpR6cEVkg/BA_ART_NoLimits%28tunecore%29.jpg" alt="Picture of Profile" className="img-fluid"/>
+                                    </div>
+                                </li>
+                                <li className="list-inline-item">
+                                    <div className="description">
+                                        <span>Photograph</span>
+                                        <label>Boyce Avenue</label>
+                                        <span className="more">...</span>
+                                    </div>
+                                </li>
+                              </ul>
                           </li>
                       </ul>
                     </div>
