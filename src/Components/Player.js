bee739ee1bd9b7461144a069372646a9c5835718
Raphael Andrade <raphael.andrade@ibm.com>
2020-07-17 20:39:14 -0300

    timeline

--- src/Components/Player.js
+++ src/Components/Player.js
@@ -10,24 +10,31 @@ function Player() {
                             <span className="previous">Previous<label>Photograph</label></span>
                         </div>
                         <div className="col-8">
-                            <ul className="list-inline list-icon-player">
-                                <li className="list-inline-item"><a href="#"><i class="fa fa-step-backward" aria-hidden="true"></i></a></li>
-                                <li className="list-inline-item"><a href="#"><i class="fa fa-play" aria-hidden="true"></i></a></li>
-                                <li className="list-inline-item"><a href="#"><i class="fa fa-step-forward" aria-hidden="true"></i></a></li>
-                            </ul>
-                            <ul className="list-inline list-time-line">
-                                <li className="list-inline-item">
-                                    <span>01:19</span>
-                                </li>
-                                <li className="list-inline-item">
-                                    <div className="bg-timeline">
-                                        <span></span>
+                            <div className="container-fluid">
+                                <div className="row">
+                                    <div className="col-12">
+                                        <ul className="list-inline list-icon-player">
+                                            <li className="list-inline-item"><a href="#"><i class="fa fa-step-backward" aria-hidden="true"></i></a></li>
+                                            <li className="list-inline-item"><a href="#"><i class="fa fa-play" aria-hidden="true"></i></a></li>
+                                            <li className="list-inline-item"><a href="#"><i class="fa fa-step-forward" aria-hidden="true"></i></a></li>
+                                        </ul>
                                     </div>
-                                </li>
-                                <li className="list-inline-item">
-                                    <span>01:19</span>
-                                </li>
-                            </ul>
+                                </div>
+                                <div className="row">
+                                    <div className="col-2">
+                                        <span>01:19</span>
+                                    </div>
+                                    <div className="col-7">
+                                        <div className="bg-timeline">
+                                            <span></span>
+                                        </div>
+                                    </div>
+                                    <div className="col-3">
+                                        <span>01:19</span>
+                                    </div>
+                                </div>
+                            </div>
+                            
                         </div>
                         <div className="col-2">
                             <span className="previous">Up next <label>Rise</label></span>
