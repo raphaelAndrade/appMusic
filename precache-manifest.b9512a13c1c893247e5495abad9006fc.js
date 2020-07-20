6aaad29994f20f4fef8a4ca362ea18ce5213f407
Raphael Andrade <raphael.andrade@ibm.com>
2020-07-20 18:05:46 -0300

    Updates

---
+++ precache-manifest.b9512a13c1c893247e5495abad9006fc.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d308d2d72585a9d967177d49fd86bcf3",
+    "url": "/appMusic/index.html"
+  },
+  {
+    "revision": "3df6eecdddc9a47ba220",
+    "url": "/appMusic/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "a0270fc8c4f98cdf9c3b",
+    "url": "/appMusic/static/css/main.da26bd18.chunk.css"
+  },
+  {
+    "revision": "3df6eecdddc9a47ba220",
+    "url": "/appMusic/static/js/2.23211b4e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/appMusic/static/js/2.23211b4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0270fc8c4f98cdf9c3b",
+    "url": "/appMusic/static/js/main.aca54a77.chunk.js"
+  },
+  {
+    "revision": "c087bff9d15040b3f942",
+    "url": "/appMusic/static/js/runtime-main.7a0db319.js"
+  },
+  {
+    "revision": "aeb3b04f13577554eb5c513e909eca05",
+    "url": "/appMusic/static/media/artist-picture.aeb3b04f.jpg"
+  },
+  {
+    "revision": "03a239a6cebb56f0799d0ef87c8e04c2",
+    "url": "/appMusic/static/media/bg-artist.03a239a6.png"
+  },
+  {
+    "revision": "518a2af5e3398189b73b741d36b707bd",
+    "url": "/appMusic/static/media/ozuna.518a2af5.jpg"
+  }
+]);
